79b9ca9c5479a9370cc6a39c924a3ac6e55467ff
abde955 <abde95.inf3@gmail.com>
2024-10-15 16:25:01 +0200

    project-finished

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="app">
+    <h1>Contact List</h1>
+    <button @click="addRandomContact">Add Random Contact</button>
+    <button @click="sortByName">Sort by Name</button>
+    <button @click="sortByPopularity">Sort by Popularity</button>
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in contacts" :key="contact.id">
+          <td><img :src="contact.picture" alt="contact" width="50" /></td>
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.popularity }}</td>
+          <td>{{ contact.wonOscar ? '🏆' : '' }}</td>
+          <td>{{ contact.wonEmmy ? '🏆' : '' }}</td>
+          <td><button @click="removeContact(contact.id)">Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import contactsData from './contacts.json';
+
+const contacts = ref(contactsData.slice(0, 5));
+
+const addRandomContact = () => {
+  const remainingContacts = contactsData.filter(contact => !contacts.value.includes(contact));
+  const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+  if (randomContact) {
+    contacts.value.push(randomContact);
+  }
+};
+
+const sortByName = () => {
+  contacts.value.sort((a, b) => a.name.localeCompare(b.name));
+};
+
+const sortByPopularity = () => {
+  contacts.value.sort((a, b) => b.popularity - a.popularity);
+};
+
+const removeContact = (id) => {
+  contacts.value = contacts.value.filter(contact => contact.id !== id);
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+img {
+  border-radius: 50%;
 }
 </style>
---
+++ src/contacts.js
@@ -0,0 +1,59 @@
+[
+    {
+      "id": 1,
+      "name": "Iron Man",
+      "popularity": 8.5,
+      "wonOscar": false,
+      "wonEmmy": false,
+      "picture": "https://i.imgur.com/XjM6h9V.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Spider-Man",
+      "popularity": 9.0,
+      "wonOscar": false,
+      "wonEmmy": true,
+      "picture": "https://i.imgur.com/8QK0vNh.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Captain America",
+      "popularity": 9.5,
+      "wonOscar": true,
+      "wonEmmy": false,
+      "picture": "https://i.imgur.com/l9M3tyB.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Thor",
+      "popularity": 8.8,
+      "wonOscar": true,
+      "wonEmmy": true,
+      "picture": "https://i.imgur.com/yAa3clM.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Black Widow",
+      "popularity": 7.5,
+      "wonOscar": false,
+      "wonEmmy": false,
+      "picture": "https://i.imgur.com/x3r8cMk.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Hulk",
+      "popularity": 7.0,
+      "wonOscar": false,
+      "wonEmmy": true,
+      "picture": "https://i.imgur.com/6hs1V9C.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Doctor Strange",
+      "popularity": 8.0,
+      "wonOscar": false,
+      "wonEmmy": false,
+      "picture": "https://i.imgur.com/2ZAnW5L.jpg"
+    }
+  ]
+  
